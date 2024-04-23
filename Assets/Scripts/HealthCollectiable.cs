d1a666927bf4bc1d41f38d438c947cec72eac7a3
nur <115826881+Nurnurr@users.noreply.github.com>
2024-04-24 00:18:03 +0300

    Initial commit

---
+++ Assets/Scripts/HealthCollectiable.cs
@@ -0,0 +1,22 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class HealthCollectiable : MonoBehaviour
+{
+    private void OnTriggerEnter2D(Collider2D other)
+    {
+        PlayerController controller = other.GetComponent<PlayerController>();
+
+        if (controller != null)
+        {
+            if (controller.health < controller.maxHealth)
+            {
+               controller.ChangeHealth(1); 
+               Destroy(gameObject);
+            }
+            
+        }
+    }
+}
---
+++ Assets/Scripts/PlayerController.cs
@@ -0,0 +1,97 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.InputSystem;
+
+
+
+public class PlayerController : MonoBehaviour
+{
+    
+    //Oyuncu karakter hareketiyle ilgili değişkenler
+    private Rigidbody2D rigidbody2D;
+    private Vector2 move;
+    public float speed = 3.0f;
+    public InputAction MoveAction;
+
+    
+    //Sağlık sistemine ilişkin değişkenler
+    public int health { get { return currentHealth; } }
+    private int currentHealth;
+    public int maxHealth = 5;
+
+    //Geçici yenilmezlikle ilgili değişkenler
+    public float timeInvincible = 2.0f;  //oyuncu karakterinin hasar aldıktan sonra ne kadar süre yenilmez kalacağı
+    private bool isInvincible; //oyuncu karakterinin o anda yenilmez olup olmadığını saklar
+    private float damageCooldown; //oyuncu karakteri artık yenilmez olmayana ve daha fazla hasar alabilene kadar ne kadar süre kaldığını saklar.
+    
+    
+    
+        
+        // Start is called before the first frame update
+    void Start()
+    {
+
+        MoveAction.Enable();
+        rigidbody2D = GetComponent<Rigidbody2D>();
+        currentHealth = maxHealth;
+
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+         move = MoveAction.ReadValue<Vector2>();
+        //Debug.Log(move);
+
+
+        if (isInvincible)
+        {
+            damageCooldown -= Time.deltaTime;
+            if (damageCooldown < 0)
+            {
+                isInvincible = false;
+            }
+        }
+       
+    }
+
+    private void FixedUpdate()
+    {
+        Vector2 position = (Vector2)rigidbody2D.position + move * speed * Time.deltaTime;
+        rigidbody2D.MovePosition(position);
+    }
+
+   public void ChangeHealth(int amount)
+    {
+        if (amount < 0)
+        {
+            if (isInvincible)
+            {
+                return;
+            }
+
+            isInvincible = true;
+            damageCooldown = timeInvincible;
+        }
+        
+        
+        
+        
+        currentHealth = Mathf.Clamp(currentHealth + amount, 0, maxHealth);
+        UIHandler.instance.SetHealthValue(currentHealth / (float) maxHealth);
+    }
+    
+    
+    
+    
+    
+    
+    
+    
+}
+
+
